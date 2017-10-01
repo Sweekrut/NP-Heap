5a4773f63da2ad60dcb7fb0e86705dad88de403f
ssjoshi2 <ssjoshi2@ncsu.edu>
2017-09-30 20:45:33 -0400

    Update core.c

--- kernel_module/src/core.c
+++ kernel_module/src/core.c
@@ -45,10 +45,119 @@
 
 extern struct miscdevice npheap_dev;
 
+///////////////////////////////////////// STUDENT CODE START /////////////////////////////////
+
+//mmap algo
+//1. Lock tree
+//2. find if object exists in tree
+//3. if object exists
+//4.    unlock tree
+//5.    if data is not null
+//6.        allocate kernel memory
+//7.        map this to process virtual memory
+//8.        return 0
+//7.    else
+//8.        map existing data pointer to process virtual memory
+//9.        return 0
+//10. else
+//11.   unlock tree
+//12.   return error
+
+
+/**
+ * @struct -    TreeNode
+ *
+ * @brief -     Structure of tree node
+ *
+ * @Modified by - Sweekrut and Vishal
+ */
+struct TreeNode {
+    struct mutex object_lock;
+    struct npheap_cmd object_data;
+};
+
+//extern variables from ioctl.c
+extern struct mutex tree_lock;
+extern struct radix_tree_root Objects;
+ 
+/**
+ * @brief - Function to allocate kernel memory and map it to user space
+ *
+ * @retval - 0 if successful, -EAGAIN or -ENOMEM on failure
+ * @Modified by - Sweekrut and Vishal
+ */
 int npheap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+    __u64 key;
+    struct TreeNode *tree_node = NULL;
+    int *area;
+
+    key = vma->vm_pgoff;
+
+    printk ("npheap_mmap: key = %llu\n", key);
+
+    mutex_lock(&tree_lock);
+
+    //Check if object exists
+    tree_node = (struct TreeNode*)radix_tree_lookup(&Objects, key);
+
+    mutex_unlock(&tree_lock);
+
+    //Object exists
+    if (tree_node) {
+        printk ("npheap_mmap: Got tree node for key = %llu\n", key);
+
+        //No object exist
+        if (!tree_node->object_data.size) {
+            printk ("npheap_mmap: No object in tree node for key = %llu\n", key);
+            tree_node->object_data.data = kmalloc(vma->vm_end - vma->vm_start, GFP_KERNEL);
+            printk ("npheap_mmap: After kmalloc of size %lu, for key = %llu\n", vma->vm_end-vma->vm_start, key);
+            
+            if (!tree_node->object_data.data) {
+                printk ("npheap_mmap: kmalloc for size %lu  for key = %llu failed\n", vma->vm_end - vma->vm_start, key);
+
+                return -ENOMEM;
+            }
+
+            printk ("npheap_mmap: Address %lu and shifted address %lu for key = %llu\n", (unsigned long)area, (unsigned long)virt_to_phys((void*)((unsigned long)area)), key);
+
+            if (remap_pfn_range(vma, vma->vm_start, virt_to_phys((void*)((unsigned long)tree_node->object_data.data)) >> PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot)) {
+                printk ("npheap_mmap: 1st remap for key = %llu\n failed", key);
+                kfree(tree_node->object_data.data);
+                tree_node->object_data.data = NULL;
+                return -EAGAIN;
+            }
+
+            tree_node->object_data.size = vma->vm_end - vma->vm_start;
+
+            return 0;
+
+        } else {
+        //Object already exists
+            
+            if (!tree_node->object_data.data) {
+                //this shouldnt happen
+                printk("npheap_mmap: Object size is present but pointer is NULL for key %llu\n", key); 
+                return -EAGAIN;
+            }
+
+            if (remap_pfn_range(vma, vma->vm_start, virt_to_phys((void*)((unsigned long)tree_node->object_data.data)) >> PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot)) {
+                printk ("npheap_mmap: 2nd remap for key = %llu\n failed", key);
+                return -EAGAIN;
+            }
+        }
+    
+    } else {
+    //This shouldnt happen
+        printk ("npheap_mmap: No object structure for key %llu found\n", key);
+        return -EAGAIN;
+    }
+
     return 0;
 }
+
+
+///////////////////////////////////////// STUDENT CODE END /////////////////////////////////
 
 int npheap_init(void)
 {
